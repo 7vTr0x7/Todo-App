0dc850e620802ade0abef8080311ab0bef103ad7
7vTr0x7 <7vtrox7@gmail.com>
2024-03-14 16:42:46 +0530

    events

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import "./style.css";
+
+interface Todo {
+  title: string;
+  isComplete: boolean;
+  readonly index: string;
+}
+
+const todos: Array<Todo> = [];
+
+const todosContainer = document.querySelector(
+  ".todoContainer"
+) as HTMLDivElement;
+
+const todoInput = document.getElementById("todoInput") as HTMLInputElement;
+
+const myForm = document.getElementById("myForm") as HTMLFormElement;
+
+const btn = document.getElementById("btn") as HTMLButtonElement;
+
+btn.onclick = myForm.submit;
+
+myForm.onsubmit = (e) => {
+  e.preventDefault();
+};
